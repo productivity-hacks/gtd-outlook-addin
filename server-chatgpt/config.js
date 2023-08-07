99e5890dacb815e338f493d70ef15eb35fc417cf
Akshit Singla <akshit@Akshits-MacBook-Pro.local>
2023-08-06 22:50:48 -0400

    Moving node-based server-side wrapper for openai into a separate repositor

--- server-chatgpt/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    systemPrompt: "You are an expert on the Getting Things Done framework for managing emails."
-};
--- server-chatgpt/index.js
+++
@@ -1,5 +0,0 @@
-const openai = require('./openaiWrapper.js');
-
-// Usage example:
-const userPrompt = "Tell me a joke.";
-openai.interactWithChatGPT(userPrompt);
